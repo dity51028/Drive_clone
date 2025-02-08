1ebe66649054d0aa33825f84bfa71c63bec288fe
dity51028 <dipannitamisti10@gmail.com>
2025-02-08 12:08:48 +0530

    solve the problem with user route and validation added

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const userRouter = require('./routes/userRoutes')
 app.set('view engine','ejs')
 
 app.use('/user',userRouter);
+app.use(express.json);
+app.use(express.urlencoded({extended:true}))
 /*
 app.get('/',(req,res)=>{
     //res.send('hello world');
@@ -12,13 +14,6 @@ app.get('/',(req,res)=>{
 
 })*/
 
-app.get('/register',(req,res)=>{
-    res.render('register');         //this route is for showing the form 
-})
-
-app.post('/register',(req,res)=>{
-
-})
 
 app.listen(3000,()=>{
     console.log('server is running on port 3000');
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const { body,validationResult } = require('express-validator');
 
 router.get('/test',(req,res)=>{
     res.send('user router created');
+})
+
+router  .get('/register',(req,res)=>{
+    res.render('register');         //this route is for showing the form 
+})
+
+
+router.post('/register',
+    body('email').trim().isEmail().isLength({min:13}),
+    body('username').trim().isLength({min:3}),
+    body('password').trim().isLength({min:5}),
+    (req,res)=>{
+
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        // return res.send('Invalid data');
+        return res.status(400).json({
+            errors:errors.array(),
+            message:'data is not valid'
+        })
+    }
+
+    console.log(errors);
+    console.log(req.body);
+    res.send(errors)
 })
 
 module.exports = router;
